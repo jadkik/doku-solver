9e4fcea466d93f1665f22d899f764b15de798443
Jad Kik <jadkik94@gmail.com>
2013-04-27 12:02:47 +0300

    Added grid selection UI

--- ui.py
+++ ui.py
@@ -1,8 +1,8 @@
 from PyQt4 import QtGui, QtCore
 import sys
-from main import *
+from main import EqualityCondition, SumCondition, ProductCondition, UniqueCondition, \
+        DifferenceCondition, DivisionCondition, Grid, Block, Group
 from grids import all_grids
-from random import choice
 
 class MainWindow(QtGui.QWidget):
     
@@ -20,8 +20,18 @@ class MainWindow(QtGui.QWidget):
                 text.setText(str(cell.value))
             else:
                 text.setText("")
-            text.setPlaceholderText(cell.block.condition.symbol())
             text.setInputMask("D")
+            
+            if self.grid.sudoku:
+                try:
+                    cond = (g.condition for g in cell.groups if isinstance(g.condition, EqualityCondition)).next()
+                    text.setPlaceholderText(cond.symbol())
+                    text.setText(str(cond.value))
+                except StopIteration:
+                    text.setPlaceholderText("")
+            else:
+                text.setPlaceholderText(cell.block.condition.symbol())
+            
             text.setFixedSize(QtCore.QSize(40, 40))
             
             pal = text.palette();
@@ -31,12 +41,21 @@ class MainWindow(QtGui.QWidget):
             self.layout.addWidget(text, y, x)
             self.text[x,y] = text
         
-        self.solve = QtGui.QPushButton("Solve")
+        self.label = QtGui.QLabel(grid_name)
+        
+        buttonBox = QtGui.QDialogButtonBox()
+        self.solve = buttonBox.addButton("Solve", QtGui.QDialogButtonBox.ActionRole)
+        self.select = buttonBox.addButton("Select", QtGui.QDialogButtonBox.ActionRole)
+        self.exit = buttonBox.addButton("Exit", QtGui.QDialogButtonBox.ActionRole)
+        
         self.solve.clicked.connect(self.onSolvePressed)
+        self.select.clicked.connect(self.onSelectPressed)
+        self.exit.clicked.connect(self.onExitPressed)
         
         mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(self.label)
         mainLayout.addLayout(self.layout)
-        mainLayout.addWidget(self.solve)
+        mainLayout.addWidget(buttonBox)
         
         self.setLayout(mainLayout)
         
@@ -89,11 +108,13 @@ class MainWindow(QtGui.QWidget):
                 blk = Block(g)
             v, op = int(i[0][:-1]), i[0][-1]
             
-            try: Cond = conds[op]
+            try:
+                Cond = conds[op]
             except KeyError:
                 log("0", "#", "Unknown Condition!", op, "value", v)
                 continue
-            else: cond = Cond(v)
+            else:
+                cond = Cond(v)
             blk.set(cond)
             
             [blk.add(g.at(*cpos)) for cpos in i[1:]]
@@ -127,23 +148,82 @@ class MainWindow(QtGui.QWidget):
         self.grid.process()
         self.updateGrid()
         self.check()
+    
+    def onSelectPressed(self):
+        dlg = SelectDialog()
+        dlg.exec_()
+        if dlg.result():
+            self.close()
+    
+    def onExitPressed(self):
+        self.close()
+
+class SelectDialog(QtGui.QDialog):
+    name = None
+    
+    def __init__(self):
+        super(SelectDialog, self).__init__()
+        
+        self.filter = QtGui.QLineEdit()
+        self.filter.textEdited.connect(self.onFilter)
+        
+        self.list = QtGui.QListWidget()
+        self.list.itemActivated.connect(self.onSelect)
+        
+        buttonBox = QtGui.QDialogButtonBox()
+        ok = buttonBox.addButton(QtGui.QDialogButtonBox.Ok)
+        cancel = buttonBox.addButton(QtGui.QDialogButtonBox.Cancel)
+        
+        ok.clicked.connect(self.onOkPressed)
+        cancel.clicked.connect(self.onCancelPressed)
+        
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.filter)
+        layout.addWidget(self.list)
+        layout.addWidget(buttonBox)
+        
+        self.filter.setFocus()
+        
+        self.setLayout(layout)
+        
+        self.populate()
+    
+    def populate(self, text=None):
+        self.list.clear()
+        if text:
+            self.items = [name for name in all_grids if text in name]
+        else:
+            self.items = all_grids.keys()
+        self.list.addItems(self.items)
+        self.list.setSelectionMode(QtGui.QListWidget.SingleSelection)
+        self.list.setCurrentRow(0)
+    
+    def select(self):
+        try:
+            self.name = str(self.list.selectedItems()[0].text())
+        except IndexError:
+            self.name = None
+        else:
+            MainWindow(self.name).show()
+            self.accept()
+    
+    def onOkPressed(self):
+        self.select()
+    
+    def onCancelPressed(self):
+        self.name = None
+        self.reject()
+    
+    def onFilter(self):
+        self.populate(str(self.filter.text()))
+    
+    def onSelect(self):
+        self.select()
 
 if __name__ == "__main__":
     
-    print "Grids:"
-    for name in all_grids:
-        print "-", name
-
-    possible_names = []
-    while len(possible_names) != 1:
-        name_start = raw_input("Which one?")
-        if name_start in all_grids:
-            possible_names = [name_start]
-            break
-        possible_names = [n for n in all_grids if n.startswith(name_start)]
-    name = possible_names[0]
-    
     app = QtGui.QApplication(sys.argv)
-    MainWindow(name).show()
+    dlg = SelectDialog()
+    dlg.show()
     app.exec_()
         
